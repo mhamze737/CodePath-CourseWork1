1f810b74aec791d0424e242b253a2a9438f5828c
Hamze Moktar <mhamze739@gmail.com>
2025-01-22 09:30:26 -0600

    Initial Commit

---
+++ CodePath CourseWork/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CodePath CourseWork
+//
+//  Created by Hamze Moktar on 1/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBacckgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor (red: red, green: green, blue: blue, alpha:0.5)
+    }
+}
+
